86f2df7871b5c34b003528e5e64633a50c0fbce1
anujchaudhari81 <anujchaudhari232@gmail.com>
2024-09-30 21:55:13 -0400

    added "learning 1.0" project to learn some basic things from @codewithchris
    yt link: https://youtu.be/F2ojC6TNwws?si=mYScYT45xrHDZ6Oo

---
+++ learning 1.0/learning 1.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  learning 1.0
+//
+//  Created by Anuj Chaudhari on 2024-09-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ learning 1.0/learning 1.0/learning_1_0App.swift
@@ -0,0 +1,17 @@
+//
+//  learning_1_0App.swift
+//  learning 1.0
+//
+//  Created by Anuj Chaudhari on 2024-09-30.
+//
+
+import SwiftUI
+
+@main
+struct learning_1_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
