9527ebc523a119353ecee4d96fcf36534f6a1084
anujchaudhari81 <anujchaudhari232@gmail.com>
2024-10-01 17:44:10 -0400

    trying new project "Memorize" to learn swift from  "CS193P by Stanford's Paul Hegarty"
    yt link: https://cs193p.sites.stanford.edu/2023

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Anuj Chaudhari on 2024-10-01.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
