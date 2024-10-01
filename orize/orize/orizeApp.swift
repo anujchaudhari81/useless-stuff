95ded3961f29f72741d8d7a3b515d49716353257
anujchaudhari81 <anujchaudhari232@gmail.com>
2024-10-01 17:38:04 -0400

    added new project "Memorize" to learn swift from the class of "CS1939 by Stanford's Paul Hegarty".
    yt link: https://cs193p.sites.stanford.edu/2023

---
+++ orize/orize/orizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  orizeApp.swift
+//  orize
+//
+//  Created by Anuj Chaudhari on 2024-10-01.
+//
+
+import SwiftUI
+
+@main
+struct orizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
