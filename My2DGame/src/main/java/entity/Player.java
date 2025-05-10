5095d3b55d1a027b4b9bcb8f50dd948734b3703a
anujchaudhari81 <anujchaudhari232@gmail.com>
2025-05-10 18:07:57 -0400

    added some basic functionalities and some image resources to player.java
    1. player.java:
    
    - added (getImagePlayer()) to extract images from resource folder
    - added simple functionality to move all directions(update())
    - added (draw()) with switch-case to draw image based on movement
    
    2. entity.java:
    
    -declared 2 variable as public: 1. BuffeedImage-obj- with variables: up1,up2...right2;
    2. directions-string;

--- My2DGame/src/main/java/entity/Player.java
+++ My2DGame/src/main/java/entity/Player.java
@@ -3,7 +3,10 @@ package entity;
 import main.GamePanel;
 import main.KeyHandler;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Player extends Entity{
 
@@ -14,29 +17,70 @@ public class Player extends Entity{
 
         this.gp=gp;
         this.keyH=KeyH;
+
+        setDefaultValues();
+        getPlayerImage();
     }
     public void setDefaultValues(){
 
         x=100;
         y=100;
         speed=4;
+
+        direction="down";
+    }
+
+    public void getPlayerImage(){
+
+        try{
+            up1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/boy_up_1.png"));
+            up2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/boy_up_2.png"));
+            down1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/boy_down_1.png"));
+            down2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/boy_down_2.png"));
+            left1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/boy_left_1.png"));
+            left2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/boy_left_2.png"));
+            right1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/boy_right_1.png"));
+            right2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/boy_right_2.png"));
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
     public void update(){
 
         if(keyH.upPressed){
+            direction="up";
             y-=speed;
         } else if (keyH.downPressed) {
+            direction="down";
             y+=speed;
         } else if (keyH.leftPressed) {
+            direction="left";
             x-=speed;
         } else if (keyH.rightPressed) {
+            direction="right";
             x+=speed;
-
         }
     }
     public void draw(Graphics2D g2){
-        g2.setColor(Color.white);
-        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+
+        BufferedImage image = null;
+
+        switch(direction){
+            case "up":
+                image=up1;
+                break;
+            case "down":
+                image=down1;
+                break;
+            case "left":
+                image=left1;
+                break;
+            case "right":
+                image=right1;
+                break;
+        }
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
     }
 
 }
